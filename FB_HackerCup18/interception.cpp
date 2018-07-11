1bdef3ed9276792774867be1357b92ab6e3ea85e
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2018-07-11 11:08:21 +0100

    Q2

---
+++ FB_HackerCup18/interception.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+	int x, temp;
+	cin >> temp;
+	for (auto i = 1; i <= temp; i++) {
+		cout << "Case #" << i << ": ";
+		cin >> x;
+		int ep, np = 0;
+		for (auto j = 0; j < x; j++) {
+			cin >> ep;
+			if (0 == np) np = 1; 
+			else np = 0;
+		}
+		cin >> ep;
+		if (np != 0) {
+			cout << 1 << endl;
+			cout << 0 << endl;
+		}
+		else cout << 0 << endl;
+	}
+	return 0;
+}
